ac81d799764afada445b6bfa51be22e4249c0bed
Dhruv Prajapati <dhruvprajapati1622004@gmail.com>
2024-09-24 13:56:34 +0530

    Module Require Prac

---
+++ math.js
@@ -0,0 +1,20 @@
+exports.sum=(a,b)=>a+b;
+exports.mult=(a,b)=>a*b;
+exports.pi = 3.14;
+
+
+// var sum=(a,b)=>a+b;
+// var mult=(a,b)=>a*b;
+// const pi = 3.14;
+
+// module.exports={
+//     sum:sum,
+//     mult:mult,
+//     pi:pi
+// };
+// obj={
+//     sum:sum,
+//     mult:mult,
+//     pi:pi  
+// };
+// module.exports=obj;
---
+++ moduleRequirePrac.js
@@ -0,0 +1,3 @@
+let math=require("./math.js");
+
+console.log(math.sum(1,3));
