c5b68fafcde448e01e884977e015f6474ebf9867
Dhruv Prajapati <dhruvprajapati1622004@gmail.com>
2024-09-24 15:15:21 +0530

    directory Import using module export and require

---
+++ fruits/caffee.js
@@ -0,0 +1,4 @@
+module.exports={
+    name:"Caffee",
+    color:"brown"
+}
---
+++ fruits/index.js
@@ -0,0 +1,6 @@
+const apple=require("./apple");
+const boba=require("./boba");
+const caffee=require("./caffee");
+
+let fruits=[apple,boba,caffee];
+module.exports=fruits;
